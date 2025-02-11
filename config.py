d256f719a4b21f7df8519e27a6745ea757017d20
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-02-11 10:28:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+# config.py - Store API keys
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-BARD_API_KEY = os.getenv("BARD_API_KEY")
-PEXELS_API_KEY = os.getenv("PEXELS_API_KEY")
+# Telegram Bot API Token
+TELEGRAM_BOT_TOKEN = os.getenv("7202774752:AAF8rHHbAeFvhQRZIROXnX1lsFo6L-RBeiQ")
+
+# Google Gemini AI API Key
+GEMINI_API_KEY = os.getenv("AIzaSyCJnbD30bgcfmG_SkQ5G8eFciq-YgCBarY")
+
+# Pexels API Key
+PEXELS_API_KEY = os.getenv("uUl7VsSwswOyeLO9ay24Ij4SYIFSsnSDjix7RQEWzCGP9NQEn3dEwhsy")
